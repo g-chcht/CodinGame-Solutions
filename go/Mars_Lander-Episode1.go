f1c55c7060d57ec7462ee64631e38aad837e6c90
g-chcht <73425301+g-chcht@users.noreply.github.com>
2020-12-26 14:39:55 +0100

    Create Mars_Lander-Episode1.go

---
+++ go/Mars_Lander-Episode1.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func main() {
+    // surfaceN: the number of points used to draw the surface of Mars.
+    var surfaceN int
+    fmt.Scan(&surfaceN)
+    
+    for i := 0; i < surfaceN; i++ {
+        // landX: X coordinate of a surface point. (0 to 6999)
+        // landY: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        var landX, landY int
+        fmt.Scan(&landX, &landY)
+    }
+    for {
+        // hSpeed: the horizontal speed (in m/s), can be negative.
+        // vSpeed: the vertical speed (in m/s), can be negative.
+        // fuel: the quantity of remaining fuel in liters.
+        // rotate: the rotation angle in degrees (-90 to 90).
+        // power: the thrust power (0 to 4).
+        var X, Y, hSpeed, vSpeed, fuel, rotate, power int
+        fmt.Scan(&X, &Y, &hSpeed, &vSpeed, &fuel, &rotate, &power)
+        
+        // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+        if vSpeed < -39 {
+            power = 4
+        } else {
+            power = 0
+        }
+        fmt.Println("0", power)
+    }
+}
